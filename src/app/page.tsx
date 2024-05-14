052fa0059b5e9fb026da1f0f0aeb92388320eed1
Zeeshan <hassan922837@gmail.com>
2024-05-14 16:26:34 +0500

    errors removed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
-import Personal_details from "@/components/Personal_details";
+import Profile_details from "@/components/Profile_details";
+
 export default function Page() {
     return (
         <main>
             <h1 className="text-center font-bold text-[32px] mt-24">BRIEF PROFILE</h1>
-            <div>
-                <Personal_details />
+            <div className="mb-10">
+                <Profile_details />
             </div>
         </main>
     );
